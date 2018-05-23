6d4cd4b42639e65f076059483be7ccbcac7a7359
Lundin Matthews <lundinmatthews@me.com>
2018-05-22 20:05:00 -0500

    need the config keys.
    These can be public and will not cause any issues with being hosted on a public github account.

---
+++ src/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  initApp: {
+    apiKey: 'AIzaSyBdx5wHyA6JWrYpN3jeg5ctpIej2HvyMyA',
+    authDomain: 'vuejs-firebase-auth-8a95d.firebaseapp.com',
+    databaseURL: 'https://vuejs-firebase-auth-8a95d.firebaseio.com',
+    projectId: 'vuejs-firebase-auth-8a95d',
+    storageBucket: 'vuejs-firebase-auth-8a95d.appspot.com',
+    messagingSenderId: '879528026904'
+  }
+}
